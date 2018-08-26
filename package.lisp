07c097f23eee09a93f883dea0b8f0c610448009b
Andrey Kotlarski <m00naticus@gmail.com>
2018-08-26 20:02:37 +0300

    Initial commit.

---
+++ package.lisp
@@ -0,0 +1,5 @@
+;;;; package.lisp
+
+(defpackage #:fdbq
+  (:use #:cl)
+  (:export #:select))
---
+++ spec.lisp
@@ -0,0 +1,63 @@
+;;;; spec.lisp
+
+(in-package #:fdbq)
+
+(defvar *dbs* (make-hash-table :test 'eq))
+
+(defclass db-field ()
+  ((offset :accessor db-field-offset :initarg :offset)
+   (size :accessor db-field-size :initarg :size)))
+
+(defclass field-dim (db-field)
+  ((filter :accessor field-filter :initarg :filter :initform nil)))
+
+(defstruct spec
+  (size 0 :type fixnum)
+  (type :file :type keyword :read-only t)
+  (path #P"" :type pathname :read-only t)
+  (fields (make-hash-table :test 'eq) :type hash-table))
+
+(defun get-spec (db)
+  (gethash db *dbs*))
+
+(defun field-offset (field spec)
+  (db-field-offset (gethash field (spec-fields spec))))
+
+(defun field-size (field spec)
+  (db-field-size (gethash field (spec-fields spec))))
+
+(defmacro defspec ((name type path) &rest fields)
+  `(let* ((spec (make-spec :type ,type :path ,path))
+          (spec-fields (spec-fields spec))
+          (size 0))
+     (dolist (field-spec ',fields)
+       (if (numberp field-spec)
+           (incf size field-spec)
+           (let ((field (make-instance 'db-field :offset size
+                                                 :size (second field-spec))))
+             (setf (gethash (first field-spec) spec-fields) field)
+             (incf size (db-field-size field)))))
+     (setf (spec-size spec) size
+           (gethash ',name *dbs*) spec)))
+
+(defmethod get-field-dim ((operand string) spec)
+  (declare (ignore spec))
+  (make-instance 'field-dim :offset 0
+                            :size (length operand)
+                            :filter operand))
+
+(defmethod get-field-dim ((operand symbol) spec)
+  (let ((field (gethash operand (spec-fields spec))))
+    (make-instance 'field-dim :offset (db-field-offset field)
+                              :size (db-field-size field))))
+
+#+or
+(defspec (recordS5x :file #P"/path/to/records5.db")
+         2
+         (cxr 2)
+         (type 1)
+         (subcode 3)
+         (date_eff 6)
+         (date_disc 6)
+         20
+         (commercial_name 16))
